39ac590de49a8157c2080dbe9f72c0e42c24300a
Henrique Teixeira <112022948+henry-Tex@users.noreply.github.com>
2023-03-28 15:02:24 -0300

    teste da API de feed ok

--- src/controladores/postagens.js
+++ src/controladores/postagens.js
@@ -15,7 +15,6 @@ async function cadastrarPostagens(req,res) {
             foto.postagem_id = postagem_id;
         }
         const fotosCadastradas = await knex('postagem_fotos').insert(fotos);
-        console.log(fotosCadastradas)
         if (!fotosCadastradas){
             await knex('postagens').where({id:postagem_id}).del();
             return res.status(400).json("Não foi possível concluir a postagem.")
@@ -27,12 +26,57 @@ async function cadastrarPostagens(req,res) {
 }
 
 async function listarPostagens(req,res) {
+    const {offset} = req.query
+    const id = req.usuario.id
+    const off = offset ? offset : 0;
     try {
-        const username = req.usuario.username;
-        const usuario = localizarUsuario(username)
+        //const postagens = knex('postagens').limit(10).offset(off);
+        //puxar postagens de terceiros
+        const postagens = await knex('postagens')
+        .where('usuario_id','!=',id)
+        .limit(10)
+        .offset(off);
+        //não existindo postagem volta um array vazio
+    if (postagens===0)return res.status(200).json(postagens); 
+            //montando feed
+            //usuario
+        for (const postagem of postagens) {
+            const usuario = await knex('usuarios')
+            .where({id: postagem.usuario_id})
+            .select('imagem','username','perfil_oficial').first()
+            postagem.usuario = usuario
+            //fotos
+            const fotos = await knex('postagem_fotos')
+            .where({postagem_id: postagem.id})
+            .select('imagem')
+            postagem.fotos = fotos;
+            //curtidas
+            const curtidas = await knex('postagem_curtidas')
+            .where({postagem_id:postagem.id})
+            .select('usuario_id');
+            postagem.curtidas = curtidas.length
+            //se curtido pelo usuário logado
+            postagem.curtidoPorMin = curtidas.find (curtida => curtida.usuario_id === id) ? true : false;
+            //comentarios
+            const comentarios = await knex('postagem_comentarios as pc')
+            .leftJoin('usuarios','usuarios.id','pc.usuario_id')
+            .where({postagem_id:postagem.id})
+            .select('usuarios.username','pc.texto')
+            postagem.comentarios=comentarios;
+        }
+        return res.status(200).json(postagens)
     } catch (error) {
         return res.status(400).json(error.message)    
     }
 };
 
-module.exports={listarPostagens,cadastrarPostagens}
+module.exports={listarPostagens,cadastrarPostagens};
+
+        // const postagensCurtidas = await knex.from('postagens as p').select('p.id','p.texto','pcu.usuario_id')
+        // .join('postagem_curtidas as pcu','pcu.postagem_id','=','p.id').first();
+        // if(postagensCurtidas.usuario_id===usuarioID) console.log('oi')
+        // const postagensFotos = await knex.from('postagens as p').select('p.id','p.texto','imagem')
+        // .join('postagem_fotos as pf','pf.postagem_id','=','p.id');
+        //.join('postagem_comentarios as pco','pco.postagem_id','=','p.id');
+        // const postagens = await knex.from('postagens as p').select('*')
+        // .join('postagem_curtidas as pc','pc.postagem_id','=',"p.id");
